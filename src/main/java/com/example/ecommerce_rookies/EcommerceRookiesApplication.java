a839a6c93e4a367f9ca0cf07957c60e49e491871
19127437 <64051785+19127437@users.noreply.github.com>
2022-06-17 00:37:29 +0700

    Design UI Database and create spring boot

---
+++ src/main/java/com/example/ecommerce_rookies/EcommerceRookiesApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecommerce_rookies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceRookiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceRookiesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ecommerce_rookies/EcommerceRookiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecommerce_rookies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceRookiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
